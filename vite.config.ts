1c7bfe05ebed7129fc12b296cd8440d7b8ae85fc
MOKLx <moklojd@gmail.com>
2026-07-02 00:55:17 +0200

    fix: configure react router v7 for netlify serverless

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
+import netlifyPlugin  from "@netlify/vite-plugin-react-router";
 import { defineConfig } from "vite"
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter(), netlifyPlugin()],
 })
